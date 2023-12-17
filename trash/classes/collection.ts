f0039c4d4d0d5907a03884579b8072015fecac90
Julio Rodriguez <jircdev@gmail.com>
2023-12-17 17:30:24 -0300

    chore: remove trash folder

--- trash/classes/collection.ts
+++
@@ -1,9 +0,0 @@
-import { Collection } from '@beyond-js/reactive/entities';
-import { Class } from './item';
-
-export /*bundle*/ class Classes extends Collection {
-	item = Class;
-	constructor() {
-		super({ storeName: 'Classes', db: 'chat-api' });
-	}
-}
