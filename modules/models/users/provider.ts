e0fd4b97f06540563bd8c83e8e6554e36f3dfc7f
Carlos Azpurua <carlosazpurua1@gmail.com>
2023-12-19 12:23:48 -0400

    feat: add userProvider

---
+++ modules/models/users/provider.ts
@@ -0,0 +1,23 @@
+import { Api } from '@aimpact/chat-sdk/api';
+import config from '@aimpact/chat-sdk/config';
+import { IProvider } from '@beyond-js/reactive/entities';
+
+export class UserProvider implements IProvider {
+	#api: Api;
+
+	constructor() {
+		this.#api = new Api(config.params.apis.chat);
+	}
+
+	async load(specs) {
+		this.#api.bearer(specs.firebaseToken);
+
+		const { status, data } = await this.#api.post('/auth/login', specs);
+
+		if (!status) {
+			throw new Error('error loading chat');
+		}
+
+		return { status, data };
+	}
+}
