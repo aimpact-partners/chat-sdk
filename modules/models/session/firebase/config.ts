20d98c9aa1c84afe1bd10632abeabe691cc81b58
Julio Rodríguez <jircdev@gmail.com>
2024-02-28 15:00:11 -0300

    fix: Modify firebase initializeApp params to get the environment from the config app

--- modules/models/session/firebase/config.ts
+++ modules/models/session/firebase/config.ts
@@ -1,24 +1,26 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
+import config from '@aimpact/chat-sdk/config';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+const DEV = {
+	apiKey: 'AIzaSyBYiZcPNBky2QvNdVwgCgU_v2B7feLtbQU',
+	authDomain: 'aimpact-partners-dev.firebaseapp.com',
+	databaseURL: 'https://aimpact-partners-dev-default-rtdb.firebaseio.com',
+	projectId: 'aimpact-partners-dev',
+	storageBucket: 'aimpact-partners-dev.appspot.com',
+	messagingSenderId: '1081434267674',
+	appId: '1:1081434267674:web:9396cc23e55385b5d171a3',
+	measurementId: 'G-QLL5WFH89Y'
+};
 const CONFIG = {
-	development: {
-		apiKey: 'AIzaSyBYiZcPNBky2QvNdVwgCgU_v2B7feLtbQU',
-		authDomain: 'aimpact-partners-dev.firebaseapp.com',
-		databaseURL: 'https://aimpact-partners-dev-default-rtdb.firebaseio.com',
-		projectId: 'aimpact-partners-dev',
-		storageBucket: 'aimpact-partners-dev.appspot.com',
-		messagingSenderId: '1081434267674',
-		appId: '1:1081434267674:web:9396cc23e55385b5d171a3',
-		measurementId: 'G-QLL5WFH89Y'
-	},
+	local: DEV,
+	development: DEV,
 	testing: {
 		apiKey: 'AIzaSyDiwwiF_O_WZHyq7QrjnxqIg9EXz1vavV0',
 		authDomain: 'chat-api-test-393820.firebaseapp.com',
@@ -38,7 +40,7 @@ const CONFIG = {
 };
 
 // Initialize Firebase
-const app = initializeApp(CONFIG.development);
+const app = initializeApp(CONFIG[config.environment]);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const facebookProvider = new FacebookAuthProvider();
